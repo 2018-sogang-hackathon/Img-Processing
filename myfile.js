becccf4fc1de6b7c7fba803ccf6b99603170063b
KiyoungYoon <craclog@gmail.com>
2018-08-25 07:27:27 +0900

    commit

--- myfile.js
+++ myfile.js
@@ -2,59 +2,53 @@
 
 const request = require('request');
 var Jimp = require('jimp');
- 
-// open a file called "lenna.png"
-// Replace <Subscription Key> with your valid subscription key.
+
 const subscriptionKey = '7bd0f6be939f422bb7259e7173ae05f1';
-
-// You must use the same location in your REST call as you used to get your
-// subscription keys. For example, if you got your subscription keys from
-// westus, replace "westcentralus" in the URL below with "westus".
 const uriBase = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect';
+const output_path = 'output/';
 
-function imageProcess(imageUrl){
-    
-}
-// const imageUrl =
-    // 'https://img.huffingtonpost.com/asset/5ab1b7562000007d06eb27f0.jpeg?ops=scalefit_630_noupscale';
+function imageProcess(imageUrl) {
 
-// Request parameters.
+    const params = {
+        'returnFaceId': 'true', 
+        'returnFaceLandmarks': 'false',
+        'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
+            'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
+    };
 
-const params = {
-    'returnFaceId': 'true',
-    'returnFaceLandmarks': 'false',
-    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
-        'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
+    const options = {
+        uri: uriBase,
+        qs: params,
+        body: '{"url": ' + '"' + imageUrl + '"}',
+        headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': subscriptionKey
+        }
+    };
+    request.post(options, (error, response, body) => {
+
+        if (error) {
+            console.log('Error: ', error);
+            return;
+        }
+
+        var jsonResponse = JSON.parse(body);
+        console.log(jsonResponse.length);
+        for (var i = 0; i < jsonResponse.length; i++) {
+            var top = jsonResponse[i].faceRectangle.top - 30;
+            var left = jsonResponse[i].faceRectangle.left - 30;
+            var width = jsonResponse[i].faceRectangle.width + 60;
+            var height = jsonResponse[i].faceRectangle.height + 60;
+
+            Jimp.read(imageUrl, ((i, top, left, width, height) => (err, lenna) => {
+                if (err) throw err;
+                lenna
+                    .crop(left, top, width, height)
+                    .writeAsync(output_path + i.toString() + '.jpg'); // save
+            })(i, top, left, width, height));
+        }
+    });
 };
 
-const options = {
-    uri: uriBase,
-    qs: params,
-    body: '{"url": ' + '"' + imageUrl + '"}',
-    headers: {
-        'Content-Type': 'application/json',
-        'Ocp-Apim-Subscription-Key' : subscriptionKey
-    }
-};
-
-request.post(options, (error, response, body) => {
-    if (error) {
-    console.log('Error: ', error);
-    return;
-    }
-    var jsonResponse = JSON.parse(body);
-
-    for (var i=0 ; i<jsonResponse.length ; i++) {
-        var top = jsonResponse[i].faceRectangle.top;
-        var left = jsonResponse[i].faceRectangle.left;
-        var width = jsonResponse[i].faceRectangle.width;
-        var height = jsonResponse[i].faceRectangle.height;
-
-        Jimp.read(imageUrl, ((i, top, left, width, height) => (err, lenna) => {
-            if (err) throw err;
-            lenna
-                .crop(left, top, width, height)
-                .writeAsync(i.toString() + '.jpg'); // save
-        })(i, top, left, width, height));
-    }
-});
+// Test usage.
+imageProcess('http://www.topstarnews.net/news/photo/first/201509/img_153804_1.jpg');
